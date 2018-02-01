44e58b9537736b3ededcbb349da06a07f8f3291a
christophersansone <chris@voltage.io>
2018-02-01 10:33:38 -0500

    part 1

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -1,13 +1,33 @@
 class Tester
   class T1
     def palindrome?(string)
-      # first implementation
+      return false if !valid?(string)
+      s = sanitize(string)
+      s == s.reverse
     end
+
+    def sanitize(string)
+      string.to_s.gsub(/[^a-zA-Z]/, '').downcase
+    end
+
+    def valid?(string)
+      string.is_a?(String) && string.length > 0
+    end
+
   end
 
-  class T2
+  class T2 < T1
     def palindrome?(string)
-      # second implementation
+      return false if !valid?(string)
+      s = sanitize(string)
+      i = 0
+      l = s.length
+      while i < l
+        return false if s[i] != s[l - i - 1]
+        i += 1
+      end
+      return true
     end
+
   end
 end
